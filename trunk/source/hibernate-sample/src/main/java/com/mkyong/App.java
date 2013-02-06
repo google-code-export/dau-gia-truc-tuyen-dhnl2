ac676faedc37276e29e09529cc02cc81d9bdbae1
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4af02f46-1396-88c0-6dd9-f482a2250175>
2013-02-06 04:17:59 +0000

    - update select query

--- trunk/source/hibernate-sample/src/main/java/com/mkyong/App.java
+++ trunk/source/hibernate-sample/src/main/java/com/mkyong/App.java
@@ -2,6 +2,7 @@ package com.mkyong;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import org.hibernate.Query;
@@ -16,10 +17,19 @@ import org.hibernate.cfg.Configuration;
 public class App {
 	public static void main(String[] args) {
 //		insertSample();
-		
+		selectSample();
 	}
 	
 	private static void selectSample(){
+		SessionFactory sessionFac = loadConfiguration().buildSessionFactory();
+		Session session = sessionFac.openSession();
+		session.beginTransaction();
+		
+		Query query = session.createQuery("from DBUser "); // table name is Class Name
+		List list = query.list();
+		System.out.println(String.format("list user %s", list.toString()));
+		session.getTransaction().commit();
+		session.close();
 	}
 	
 	private static void insertSample(){

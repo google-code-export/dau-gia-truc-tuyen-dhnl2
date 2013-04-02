368aaad57c2259881f35a0ece1b9319af385327b
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4af02f46-1396-88c0-6dd9-f482a2250175>
2013-04-02 04:47:44 +0000

    add manven

---
+++ trunk/source/musicpage/source/frontend/core/src/main/webapp/js/IndexModel.js
@@ -0,0 +1,31 @@
+function IndexModel () {
+    this.selectedIndexSong = 0,
+    this.createOfflineData = function(){
+        var srcO = "http://data.chiasenhac.com/downloads/1078/1/1078000-8a14e570/320/Sun%20-%20Belinda%20Carlisle%20[MP3%20300kbps].mp3";
+        this.playingSongsList = [
+            {title:"Just dance - LadyGaga",src:srcO},
+            {title:"Poker face - LadyGaga",src:srcO},
+            {title:"Bad romance - LadyGaga",src:"http://,..."},
+            {title:"Paparazzi - LadyGaga",src:"http://,..."},
+            {title:"TelePhone - LadyGaga",src:"http://,..."},
+            ];
+
+        this.songsResultList = [
+            {title:"jjjjjjjjjjjj",src:"http://,..."},
+            {title:"5555515151",src:"http://,..."},
+            {title:"15151515",src:"http://,..."},
+            {title:"3312341212",src:"http://,..."},
+            {title:"7778787878",src:"http://,..."},
+            {title:"09090909090909",src:"http://,..."},
+        ];
+
+        this.categories = [
+            {title:"POP",link:"http://,..."},
+            {title:"ROCK",link:"http://,..."},
+            {title:"DAN CA",link:"http://,..."},
+            {title:"RAP",link:"http://,..."},
+            {title:"KHAC",link:"http://,..."},
+        ];
+    }
+}
+

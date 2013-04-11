21de7e32d5e939a9bfb60c74704002ec5244a133
tuanlhdnl@gmail.com <tuanlhdnl@gmail.com@4af02f46-1396-88c0-6dd9-f482a2250175>
2013-04-11 08:05:11 +0000

    add module

---
+++ trunk/source/musicpage/source/frontend/bom/src/main/webapp/js/controller/TopAlbumController.js
@@ -0,0 +1,24 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: tuanlhd
+ * Date: 4/8/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function TopAlbumController(){
+    this.init = function(){
+
+    },
+    this.createModel = function(){
+        var model = new TopAlbumModel();
+        model.createOfflineData();
+        return model;
+    },
+    this.onClick = function(){
+
+    },
+    this.createView = function(){
+        return new TopAlbumView();
+    }
+}
+BaseController.inherits(TopAlbumController);
---
+++ trunk/source/musicpage/source/frontend/bom/src/main/webapp/js/model/TopAlbumModel.js
@@ -0,0 +1,24 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: tuanlhd
+ * Date: 4/8/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function TopAlbumModel (){
+    this.createOfflineData = function(){
+        this.listAlbum = [
+            new AlbumTop(105,214,0,0),
+            new AlbumTop(237,94,271,0),
+            new AlbumTop(180,88,271,253),
+            new AlbumTop(90,90,280,448),
+            new AlbumTop(328,92,0,63),
+            new AlbumTop(78,78,0,894),
+            new AlbumTop(105,214,112,0),
+            new AlbumTop(105,214,112,0),
+            new AlbumTop(105,214,112,0),
+
+        ]
+
+    }
+}

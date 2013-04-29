a2d69ddb969603506aa89948f106dffdfbdbcd55
TuanLHDNL@gmail.com <TuanLHDNL@gmail.com@4af02f46-1396-88c0-6dd9-f482a2250175>
2013-04-29 11:26:50 +0000

    - new features

---
+++ trunk/source/musicpage/source/frontend/core/src/main/webapp/js/controller/VolumeController.js
@@ -0,0 +1,44 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: letuan
+ * Date: 4/29/13
+ * Time: 5:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function VolumeController(){
+    this.init = function(){
+
+    }
+
+    this.createView = function(){
+        return new VolumeView();
+    }
+
+    this.onClick = function(e){
+
+    }
+}
+BaseController.inherits(VolumeController);
+
+//the code below is a plugin
+//length: how many bars
+//height: height of the tallest bar
+//nowselected: which bar is selected
+function drawvolumecontroller(length,height,nowselected){
+	document.getElementById("volumcontroller").innerHTML = "";
+	for (i=0;i<length;i++){
+		magassag = 7 + Math.round((1.4)*(length - i)); //where 40 is the container height
+		margintop = height-magassag;
+		if (margintop <= 0) {margintop=0;}
+		if (i >= nowselected){		//background-color valtozik ameddig epp ki van jelolve
+			document.getElementById("volumcontroller").innerHTML = document.getElementById("volumcontroller").innerHTML+'<div  onmouseup="volumecontrolchanged('+i+')" style="background-color:#898989;height:'+magassag+'px;margin-top:'+margintop+'px;" class="volumecontrollerbar"></div>';
+		} else {
+			document.getElementById("volumcontroller").innerHTML = document.getElementById("volumcontroller").innerHTML+'<div  onmouseup="volumecontrolchanged('+i+')" style="height:'+magassag+'px;margin-top:'+margintop+'px;" class="volumecontrollerbar"></div>';
+		}
+	}
+}
+function volumecontrolchanged(newvolume){
+	drawvolumecontroller(20,35,newvolume);
+	document.getElementById("volumeindicator").innerHTML = newvolume;
+//	alert(newvolume);
+}

c1afdb0aefb30ff13b31d48355cd61096db13cb3
TuanLHDNL@gmail.com <TuanLHDNL@gmail.com@4af02f46-1396-88c0-6dd9-f482a2250175>
2013-09-16 15:38:44 +0000

    new shop api

---
+++ source/musicpage/source/backend/service/src/main/java/com/entertainment/musicpage/rest/ShopApi.java
@@ -0,0 +1,43 @@
+package com.entertainment.musicpage.rest;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.entertainment.musicpage.dao.models.Song;
+import com.entertainment.musicpage.daoimplement.CustomSongDAO;
+import com.entertainment.musicpage.daoimplement.CustomSongDAOImpl;
+import com.entertainment.musicpage.dto.Product;
+import com.entertainment.musicpage.dto.ResponseShop;
+import com.thetransactioncompany.cors.CORSFilter;
+
+@Path("/shop")
+public class ShopApi {
+	@GET
+	@Path("/topdown")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ResponseShop getTopDownProducts(@QueryParam("start")Integer start, @QueryParam("amount")Integer amount) {
+
+		
+		CustomSongDAO dao = new CustomSongDAOImpl();
+		List<Song > lSongs = dao.findOrderById(start, amount);
+		ArrayList<Product> lProducts = new ArrayList<Product>();
+		Iterator<Song> it = lSongs.iterator();
+		while(it.hasNext()){
+			Song s = it.next();
+			Product p = new Product(s.getTitle(), s.getCreatedTime().toString(), s.getDescription(), 99);
+			lProducts.add(p);
+		}
+		
+		ResponseShop resShop = new ResponseShop();
+		resShop.setListProducts(lProducts);
+		
+		return resShop;
+	}
+}
